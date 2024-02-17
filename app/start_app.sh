bef86ae44296faf9be94755b92d76cef108ed5cd
RuKapSan <mokan.956@gmail.com>
2024-02-17 19:05:52 +0200

    Update start_app.sh script to run FastAPI server and nginx concurrently

--- app/start_app.sh
+++ app/start_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 cd /app
-
 # Запустите сервер FastAPI
-uvicorn main:app --host 0.0.0.0 --port 3001
+uvicorn main:app --host 0.0.0.0 --port 3001 &
+# Запустите nginx
+nginx -g 'daemon off;'
